03d18b5a03fa0cdd37bc30f09e0086b10bb10ded
ahsan200t <ahsanpekua200@gmail.com>
2024-06-10 00:03:19 +0600

    Update Methode Successfully Done

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+// const cookieParser = require('cookie-parser');
+// const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+//Must remove "/" from your production URL
+const corsOptions = {
+    origin: ['http://localhost:5173', 'http://localhost:5000'],
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
 app.use(express.json());
+// app.use(cookieParser())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ouoa8yh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -26,8 +35,34 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         const donationRequestCollection = client.db('bloodDonationDb').collection('donationRequest');
-        // Donation Request Related Api
+        // Auth Related Api
+        // app.post('/jwt', async (req, res) => {
+        //     const user = req.body
+        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        //         expiresIn: '365d',
+        //     })
+        //     res.cookie('token', token, {
+        //             httpOnly: true,
+        //             secure: process.env.NODE_ENV === 'production',
+        //             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        //         })
+        //         .send({ success: true })
+        // })
+        // // Logout
+        // app.get('/logout', async (req, res) => {
+        //     try {
+        //         res.clearCookie('token', {
+        //                 maxAge: 0,
+        //                 secure: process.env.NODE_ENV === 'production',
+        //                 sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        //             }).send({ success: true })
+        //         console.log('Logout successful')
+        //     } catch (err) {
+        //         res.status(500).send(err)
+        //     }
+        // })
 
+        // Donation Request Related Api
         app.post('/donation-request', async (req, res) => {
             const requestData = req.body;
             const result = await donationRequestCollection.insertOne(requestData);
@@ -40,20 +75,54 @@ async function run() {
             const result = await donationRequestCollection.find(query).toArray()
             res.send(result)
         })
+        app.get('/my-recent-request/:email', async (req, res) => {
+            const email = req.params.email;
+            let query = { 'donor.email': email };
+            const result = await donationRequestCollection.find(query).toArray()
+            res.send(result)
+        })
 
         app.get('/donation-request', async (req, res) => {
             const request = req.body;
             const result = await donationRequestCollection.find(request).toArray();
             res.send(result)
         })
-        
+
         // Donation Details Get
         app.get('/donation-details/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
+
             const result = await donationRequestCollection.findOne(query);
             res.send(result);
-          })
+        })
+        // Donation Request Update
+        app.put('/update/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updatedRequest = req.body;
+            const request = {
+                $set:{
+                    recipient: updatedRequest.recipient,
+                    district: updatedRequest.district,
+                    upazila: updatedRequest.upazila,
+                    date: updatedRequest.date,
+                    donor: updatedRequest.donor,
+                    status: updatedRequest.status,
+
+                }
+            }
+            const result = await donationRequestCollection.updateOne(filter,request,options);
+            res.send(result)
+        })
+
+        app.get('/update/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await donationRequestCollection.findOne(query);
+            res.send(result);
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
