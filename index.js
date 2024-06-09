fef8b2dd75c2c43546cd86a814d4ddd90aa2f460
ahsan200t <ahsanpekua200@gmail.com>
2024-06-10 02:00:00 +0600

    delete methode done

--- index.js
+++ index.js
@@ -123,6 +123,13 @@ async function run() {
             const result = await donationRequestCollection.findOne(query);
             res.send(result);
         })
+        // recent Donation section delete
+        app.delete('/single-donation/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await donationRequestCollection.deleteOne(query);
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
